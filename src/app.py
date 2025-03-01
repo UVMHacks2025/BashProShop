02df1ea7c6bd7bb5fcc3393d3d2c81eb4e46874d
Levi Pare <levipare@gmail.com>
2025-03-01 10:11:56 -0500

    test routes

--- src/app.py
+++ src/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
-    return "Hello, World!"
+def listings():
+    return render_template("listings.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
