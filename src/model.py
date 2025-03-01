2b3764a466700ea9fe06618a4f063d6048d9a3de
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-03-01 10:40:40 -0500

    Finalizing user schema.

--- src/model.py
+++ src/model.py
@@ -28,4 +28,6 @@ def init_db(app: Flask) -> SQLAlchemy:
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
     hashed_password: Mapped[str]
